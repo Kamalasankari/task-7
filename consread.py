670c5b1718f3666455a5ce4c21459d82b821a8e5
Kamalasankari <142683264+Kamalasankari@users.noreply.github.com>
2023-09-02 19:41:17 +0530

    Update consread.py

--- consread.py
+++ consread.py
@@ -1,8 +1,8 @@
-file1 = open("../myfile.txt", 'w')
+file1 = open("myfile.txt", 'w')
 L = ["This the task given by guvi \nto read the content of the file \nin the console"]
 file1.writelines(L)
 file1.close()
-file1 = open("../myfile.txt", 'r')
+file1 = open("myfile.txt", 'r')
 print("Content in the file is ")
 print(file1.read())
 print()
